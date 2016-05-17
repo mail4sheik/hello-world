cc1dec44f97e06e49003581dd7b280f55039262b
Sheik Mohammed Muzammil <mail4sheik@gmail.com>
2016-05-17 19:28:55 +0530

    ui Router and ng router examples

--- com/js/mainapp.js
+++ com/js/mainapp.js
@@ -1,4 +1,4 @@
-var app = angular.module("mainApp",['ngRoute'])
+var app = angular.module("mainApp",['ngRoute','ui.router']);
 /*
 app.config(['$routeProvider',function($routeProvider){
     $routeProvider
@@ -10,21 +10,50 @@ app.config(['$routeProvider',function($routeProvider){
         
     });
 }]);*/
-app.config(['$routeProvider',
-        function($routeProvider) {
+app.config(['$routeProvider','$stateProvider', '$urlRouterProvider',
+        function($routeProvider,$stateProvider,$urlRouterProvider) {
             $routeProvider.
                 when('/secondform', {
-                    templateUrl: 'templates/secondform.html'
-                    
+                    templateUrl: 'templates/secondform/secondform.html',
+                    controller: 'secondcontroller'
                 }).
-                when('/route2', {
-                    templateUrl: 'angular-route-template-2.jsp'
+                when('/thirdform', {
+                    templateUrl: 'templates/thirdform/thirdform.html'
                    
+                });
+/*                .otherwise({
+                    templateUrl: 'templates/fourthform/fourthform.html'
+                    
+                });*/
+            $stateProvider
+                .state('state1',{
+                    url:"/state1",
+                    templateUrl:"states/state1.html"
+                    
+                })
+                .state('state2',{
+                    url:"/state2",
+                    templateUrl:"states/state2.html"
+                    
+                })
+                .state('state1.list',{
+                    url:"/list",
+                    templateUrl:"states/state1.list.html",
+                    controller:function($scope){
+                        $scope.items = ["A","List","Of","items"];
+                    }
+                })
+                .state('state2.list',{
+                    url:"/list",
+                    templateUrl:"states/state2.list.html",
+                    controller:function($scope){
+                        $scope.things=["A","Set","Of","Things"];
+                    }
                 });
         }]);
 app.directive("headerDirective",function(){
     return{
         template:"User Name Table"
     };
-    
+
 });
---
+++ templates/secondform/seconformcontroller.js
@@ -0,0 +1,3 @@
+app.controller('secondcontroller', ['$scope', function($scope) {
+    $scope.firstModel  = "This is first model from controller";
+}]);
